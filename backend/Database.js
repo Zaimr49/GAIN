f798a1c57e7ebcd01f23bf8e01751b099aeb4022
Aakash Venkatraman <aakashvenkat29@gmail.com>
2024-06-22 13:53:45 +0530

    Update Database.js

--- backend/Database.js
+++ backend/Database.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://GAIN:<password>@aakash29.g1vvkpf.mongodb.net/?retryWrites=true&w=majority&appName=aakash29";
+const uri = "mongodb+srv://GAIN:<GAIN>@aakash29.g1vvkpf.mongodb.net/?retryWrites=true&w=majority&appName=aakash29";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
