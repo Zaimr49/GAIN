1528e09696627596f3b69ec8523a7c0f7b87bc4e
Manasvi Agrawal <manasvi.g.agrawal@gmail.com>
2024-06-23 11:48:23 +0530

    login and auth

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const session = require('express-session');
+const passport = require('./auth');
 const { connectToDatabase } = require('./Database');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,17 +11,29 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+// Initialize session middleware
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect to MongoDB and then start the server
 connectToDatabase().then((client) => {
   // Make the MongoDB client available in the request object
   app.use((req, res, next) => {
     req.dbClient = client;
-    req.db = client.db('investment_advisor_db'); // Replace with your database name, idk the name yet?
+    req.db = client.db('userdetails'); // Replace with your database name
     next();
   });
 
-  // Mount user routes
+  // Mount routes
   app.use('/api/users', userRoutes);
+  app.use(authRoutes);
 
   // Start the server
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
